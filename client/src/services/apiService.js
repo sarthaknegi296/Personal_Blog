8a5aacdce1094ef60375ae89ab83caa85f611d69
Sarthak Negi <sarthaknegi296@gmail.com>
2025-09-08 12:25:58 +0530

    configure dynamic API URL for frontend

--- client/src/services/apiService.js
+++ client/src/services/apiService.js
@@ -1,11 +1,19 @@
 import axios from "axios";
 
-const apiService = axios.create({
+const token = localStorage.getItem('jwt');
 
+const VITE_API_URL = import.meta.env.VITE_API_URL;
+
+const apiService = axios.create({
   // This means that for any request made with this instance,
   // 'http://localhost:5000/api' will be prepended to the URL.
   // For example, apiService.get('/posts') will make a GET request to 'http://localhost:5000/api/posts'.
-  baseURL: "http://localhost:5000/api",
+  baseURL: VITE_API_URL,
+  headers: {
+    "Content-Type": "application/json",
+    // 3. Conditionally add the Authorization header if a token exists.
+    ...(token && { Authorization: `Bearer ${token}` }),
+  },
 });
 
 // This function will be called for EVERY request made using this 'apiService' instance.
